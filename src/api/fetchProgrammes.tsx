2e9290af01c43bc5cb7ccc741612b90b7cafc6e5
Sergey Vorobev <radio.vorobev@yandex.ru>
2025-03-23 22:34:37 +0300

    progs order && background color

--- src/api/fetchProgrammes.tsx
+++ src/api/fetchProgrammes.tsx
@@ -4,7 +4,8 @@ const fetchProgrammes = async () => {
   const { data, error } = await supabase
     .from('programmes')
     .select('*')
-    .eq('is_active', 'TRUE');
+    .eq('is_active', 'TRUE')
+    .order('sort_order', { ascending: true });
 
   if (error) {
     throw error;
